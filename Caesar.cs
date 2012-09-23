3440837d4a8ad96420d4e03f71fb90ac09b2d823
Arjen van der Ende <git@arjenvanderende.nl>
2012-09-23 17:51:17 +0200

    Ported Java implementation to C#

---
+++ Caesar.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pyroxene.Kata.Caesar
+{
+	public class Caesar
+	{
+		private const int CharactersInAlphabet = 26;
+		private const char FirstCharInAlphabet = 'A';
+		private const char LastCharInAlphabet = 'Z';
+
+		public string Encode(string text, int offset) {
+			EnsureValidInput(text);
+			StringBuilder result = new StringBuilder();
+			foreach (char c in UpperCasedCharacters(text)) {
+				result.Append(CaesarCharacter(c, offset));
+			}
+			return result.ToString();
+		}
+
+		private void EnsureValidInput(string text) {
+			if (UpperCasedCharacters(text).Any(c => c < FirstCharInAlphabet || c > LastCharInAlphabet)) {
+				throw new ArgumentException("text is invalid");
+			}
+		}
+
+		private IEnumerable<char> UpperCasedCharacters(string text) {
+			return text.ToUpperInvariant().ToCharArray();
+		}
+
+		private int Normalize(int offset) {
+			return offset % CharactersInAlphabet;
+		}
+
+		private char CaesarCharacter(char c, int offset) {
+			return WrapCharacter(OffsetCharacter(c, offset));
+		}
+
+		private char OffsetCharacter(char c, int offset) {
+			return (char)(c + Normalize(offset));
+		}
+
+		private char WrapCharacter(char character) {
+			if (character > LastCharInAlphabet) {
+				return (char)(character - CharactersInAlphabet);
+			}
+			if (character < FirstCharInAlphabet) {
+				return (char)(character + CharactersInAlphabet);
+			}
+			return character;
+		}
+	}
+}
---
+++ CaesarTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Pyroxene.Kata.Caesar
+{
+	[TestFixture]
+	public class CaesarTest
+	{
+		[Test]
+		public void ReturnsStringWithRot0() {
+			Caesar caesar = makeCeasar();
+			Assert.AreEqual("A", caesar.Encode("A", 0));
+		}
+
+		[Test]
+		public void ReturnsUpperCaseInput() {
+			Caesar caesar = makeCeasar();
+			Assert.AreEqual("A", caesar.Encode("a", 0));
+		}
+
+		[Test]
+		public void ReturnsInputWithOffsetOfOne() {
+			Caesar caesar = makeCeasar();
+			Assert.AreEqual("B", caesar.Encode("A", 1));
+		}
+
+		[Test]
+		public void ReturnsInputWithTwoCharactersWithOffsetOfOne() {
+			Caesar caesar = makeCeasar();
+			Assert.AreEqual("BC", caesar.Encode("AB", 1));
+		}
+
+		[Test]
+		public void ReturnsWrappedCharacterGivenOffsetForInputExceedsCharacterSet() {
+			Caesar caesar = makeCeasar();
+			Assert.AreEqual("A", caesar.Encode("Z", 1));
+		}
+
+		[Test]
+		public void ReturnsWrappedCharacterGivenNegativeOffsetForInputExceedsCharacterSet() {
+			Caesar caesar = makeCeasar();
+			Assert.AreEqual("Z", caesar.Encode("A", -1));
+		}
+
+		[Test]
+		public void ReturnsWrappedCharacterGivenCharacterWrapsMultipleTimes() {
+			Caesar caesar = makeCeasar();
+			Assert.AreEqual("B", caesar.Encode("A", 53));
+		}
+
+		[Test, ExpectedException(typeof(ArgumentException))]
+		public void ThrowsGivenInputIsContainsInvalidCharacters() {
+			const string invalidCharacter = "!";
+			Caesar caesar = makeCeasar();
+			caesar.Encode(invalidCharacter, 53);
+		}
+
+		// WRONG INPUT
+
+		private Caesar makeCeasar() {
+			return new Caesar();
+		}
+	}
+}
